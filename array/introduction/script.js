85947f09a8c4c9cba56672fb9612874acdd2cafa
Dhruvika Purohit <purohitdhruvika87@gmail.com>
2025-05-12 16:23:51 +0530

    Add files via upload

---
+++ array/introduction/script.js
@@ -0,0 +1,53 @@
+// print the array
+// let array=[1,2,3,4,5,6,7,8,9,10];
+// for(let i=0; i<array.length; i++)
+// {
+//     document.writeln(array[i]);
+// }
+
+// print only even numbers of array - filter
+// num=[77,89,88,76,600,45,23,12,11,10,9,8,7,6,5,4,3,2,1];
+// for(let i=0;i<num.length;i++)
+// {
+//     if(num[i]%2==0)
+//     {
+//         document.writeln(num[i]);
+//     }
+// }
+
+
+// show products which price is more than 100.
+// price=[100,60,200,399,45,760,90,80,100,750];
+// for(let i=0;i<price.length;i++)
+// {
+//     if(price[i]>100)
+//     {
+//         document.writeln(price[i]);
+//     }
+// }
+
+// print sum of array.
+// a=[7,54,2,9,8,6,5,4,3,2,1];
+// sum=0;
+// for(let i=0;i<a.length;i++)
+// {
+//     sum+=a[i];
+// }
+// document.writeln(sum);
+
+
+// printf reverse array.
+// array=[1,2,3,4,5,6,7,8,9,10];
+// for(let i=array.length-1;i>=0;i--)
+// {
+//     document.writeln(array[i]);
+// }
+
+
+//Perform addition between two array.
+a=[1,2,3,4,5];
+b=[6,7,8,9,10];
+for(let i=0;i<a.length;i++)
+{
+    document.writeln(a[i]+b[i]);
+}
